846a01d788808b3978812348b4ed6ee87a09098c
Anand pratap singh <47928185+singhanand453@users.noreply.github.com>
2019-09-27 23:46:00 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@ try:
 except ImportError:
     from distutils.core import setup
 
-with open("README.md", 'r') as f:
+with open("README.Md", 'r') as f:
     long_description = f.read()
 
-with open('requirements.txt', 'r') as f:
+with open('requirement.txt', 'r') as f:
     requirements = [line.strip() for line in f.readlines()]
 
 setup(
-   name='Wiki_Scraper',
+   name='Wiki_scraper',
    version='1.0.0',
-   description="Simple python web scraping tool to scrap educational institutions details from Wikipedia using user input.",
+   description="Simple Python web scraping tool to scrap educational institutions details from Wikipedia using user input.",
    long_description=long_description,
    url='https://github.com/DSC-Galgotias/Wiki_Scraping.git',
    packages=['Wiki_Scraping'],
